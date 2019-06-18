239974962f5414c142c3f2df37ecf65078632c22
frylock34 <frylock3434@gmail.com>
2019-06-18 17:49:36 +0400

    add images

---
+++ pages/ste520.vue
@@ -0,0 +1,21 @@
+<template>
+  <PrinterBanner model="ste520" :description="$t('printer.ste520.description')"/>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'nuxt-property-decorator'
+import PrinterBanner from '~/components/printers/PrinterBanner.vue'
+
+@Component({
+  components: {
+    PrinterBanner
+  }
+})
+export default class Ste520 extends Vue {
+
+}
+
+</script>
+
+<style>
+</style>
