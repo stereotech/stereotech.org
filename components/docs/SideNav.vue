093fac12a5f335cea3fafb3b8a94abb54a67e424
frylock34 <frylock3434@gmail.com>
2019-11-22 16:40:14 +0400

    fix valert style

--- components/docs/SideNav.vue
+++ components/docs/SideNav.vue
@@ -1,22 +1,23 @@
 <template>
-  <v-list nav v-model="item">
-    <template v-for="(group, index) in list">
-      <v-list-item :key="`title-${index}`">
-        <v-list-item-title class="title">{{ group.title }}</v-list-item-title>
-      </v-list-item>
-      <v-divider :key="`divider-${index}`"></v-divider>
-      <template v-for="link in group.links">
-        <v-list-item :key="link.to">
-          <v-list-item-title class="font-weight-bold" nuxt :to="link.to">{{ link.name }}</v-list-item-title>
+  <v-card>
+    <v-list nav v-model="item">
+      <template v-for="(group, index) in list">
+        <v-list-item :key="`title-${index}`">
+          <v-list-item-title class="title">{{ group.title }}</v-list-item-title>
         </v-list-item>
-        <v-list-item
-          v-for="content in link.contents"
-          :key="content.to"
-          nuxt
-          :to="`/manuals${menuSection}${link.to}${content.to}`"
-        >{{ content.name }}</v-list-item>
-      </template>
-      <!--<v-list-group value="true">
+        <v-divider :key="`divider-${index}`"></v-divider>
+        <template v-for="link in group.links">
+          <v-list-item :key="link.to">
+            <v-list-item-title class="font-weight-bold" nuxt :to="link.to">{{ link.name }}</v-list-item-title>
+          </v-list-item>
+          <v-list-item
+            v-for="content in link.contents"
+            :key="content.to"
+            nuxt
+            :to="`/manuals${menuSection}${link.to}${content.to}`"
+          >{{ content.name }}</v-list-item>
+        </template>
+        <!--<v-list-group value="true">
         <template v-slot:activator>
           
         </template>
@@ -25,9 +26,10 @@
           :key="content.to"
           @click="goTo(`/manuals${menuSection}${link.to}${content.to}` ,content.to)"
         >{{ content.name }}</v-list-item>
-      </v-list-group>-->
-    </template>
-  </v-list>
+        </v-list-group>-->
+      </template>
+    </v-list>
+  </v-card>
 </template>
 
 <script lang="ts">
