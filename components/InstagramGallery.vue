4ab25e5fb86189eabbcd9010d17b7c33722eea6a
Ilya Gushchin <frylock3434@gmail.com>
2020-03-14 19:57:50 +0400

    add legal and and sciense page

--- components/InstagramGallery.vue
+++ components/InstagramGallery.vue
@@ -3,8 +3,8 @@
     <v-row>
       <v-col cols="12">
         <v-card href="https://instagram.com/ste3d_ru/" target="_blank">
-          <v-img class="align-end" src="/services/insta_feed.jpg">
-            <v-card-title>Примеры работ в нашем Instagram @ste3d_ru</v-card-title>
+          <v-img height="300" src="/services/insta_feed.jpg">
+            <v-card-title class="white--text">Примеры работ в нашем Instagram @ste3d_ru</v-card-title>
           </v-img>
         </v-card>
       </v-col>
