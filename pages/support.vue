75bfdf6b5b54b06c53571ab86385eaa8d2a37640
Kate <katya.romakh@yandex.ru>
2020-03-17 21:29:57 +0400

    add contact form

--- pages/support.vue
+++ pages/support.vue
@@ -1,11 +1,17 @@
-<template></template>
+<template>
+<ContactForm/>
+</template>
 
 <script lang="ts">
 import { Vue, Component } from 'vue-property-decorator'
+import ContactForm from '~/components/ContactForm.vue'
 
 @Component({
   head: {
     title: 'Техническая поддержка'
+  },
+  components:{
+    ContactForm
   }
 })
 export default class Support extends Vue {
