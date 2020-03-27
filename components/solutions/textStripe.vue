216a9db5a93bf6ab31922e6687d3997c44fd3dc4
Kate <katya.romakh@yandex.ru>
2020-03-27 23:10:24 +0400

    started to do blog page, continued to do production (manufacturing) page

---
+++ components/solutions/textStripe.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card dark color="primary">
+      <p class="text-center" id="str">
+        <slot></slot>
+      </p>
+
+  </v-card>
+      
+</template>
+
+<script>
+import { Vue, Component} from 'vue-property-decorator'
+@Component
+export default class textStripe extends Vue{
+
+}
+</script>
+
+<style>
+    #str{
+        font-size: 25pt
+    }
+</style>
