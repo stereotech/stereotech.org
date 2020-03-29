6be9394d68aaa78cb2a40676a80efec140fc56cc
Ilya Gushchin <frylock3434@gmail.com>
2020-03-29 16:39:22 +0400

    edit specs page

--- components/printers/CompareSpecs.vue
+++ components/printers/CompareSpecs.vue
@@ -1,173 +1,79 @@
 <template>
-  <v-col cols="12">
-    <v-card>
-      <v-container>
-        <v-row>
-          <v-col cols="12">
-            <v-data-table :items="items" :headers="headers" hide-default-footer item-key="id">
-              <template v-slot:item="{item}">
-                <tr>
-                  <td
-                    class="subheading text-uppercase font-weight-light text-center"
-                  >{{ item.title }}</td>
-                  <td class="subheading text-center">{{ item.ste520 }}</td>
-                  <td class="subheading text-center">{{ item.ste320 }}</td>
-                </tr>
-              </template>
-              <template v-slot:footer>
-                <v-container>
-                  <v-row>
-                    <v-col offset-sm="4" cols="4">
-                      <v-btn color="primary" nuxt to="/ste520">{{ $store.state.lang.know_more }}</v-btn>
-                      <v-btn
-                        color="success"
-                        href="https://ste3d.ru/ste520"
-                        target="_blank"
-                      >{{ $store.state.lang.printer.ste520.state }}</v-btn>
-                    </v-col>
-                    <v-col cols="4">
-                      <v-btn color="primary" nuxt to="/ste320">{{ $store.state.lang.know_more }}</v-btn>
-                      <v-btn
-                        color="success"
-                        href="https://ste3d.ru/ste320"
-                        target="_blank"
-                      >{{ $store.state.lang.printer.ste320.state }}</v-btn>
-                    </v-col>
-                  </v-row>
-                </v-container>
-              </template>
-            </v-data-table>
-          </v-col>
-        </v-row>
-      </v-container>
-    </v-card>
+  <v-col cols="4" v-if="$apollo.loading"></v-col>
+  <v-col lg="8" v-else>
+    <v-container>
+      <v-row justify="center">
+        <v-col cols="12" v-for="product in products" :key="product.product_id">
+          <v-card>
+            <v-container>
+              <v-row>
+                <v-col cols="12" v-for="option in product.options" :key="option.product_option_id">
+                  <v-container>
+                    <v-row>
+                      <v-col
+                        cols="12"
+                        v-for="option_value in option.product_option_value"
+                        :key="option_value.product_option_value_id"
+                      >{{ option_value.name}}{{ parseFloat(option_value.price) + parseFloat(product.price) }}</v-col>
+                    </v-row>
+                  </v-container>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-col>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from 'vue-property-decorator'
+import gql from 'graphql-tag'
+
+interface CompareTableData {
+  name: string
+  price: string
+  description: string
+}
 
 @Component({
   components: {
   }
 })
 export default class CompareSpecs extends Vue {
-  private headers: any[] = [
-    {
-      text: '',
-      sortable: false,
-      value: 'title',
-      align: 'center'
-    },
-    {
-      text: 'STE 520',
-      sortable: false,
-      value: 'ste520',
-      align: 'center'
-    },
-    {
-      text: 'STE 320',
-      sortable: false,
-      value: 'ste320',
-      align: 'center'
-    }
-  ]
+  products: any = {}
 
-  private items: any[] = [
-    {
-      id: 1,
-      title: this.$store.state.lang.summary,
-      ste520: this.$store.state.lang.printer.ste520.description,
-      ste320: this.$store.state.lang.printer.ste320.description
-    },
-    {
-      id: 2,
-      title: this.$store.state.lang.printer.ste520.spec.technology.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.technology.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.technology.value
-    },
-    {
-      id: 3,
-      title: this.$store.state.lang.printer.ste520.spec.printhead.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.printhead.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.printhead.value
-    },
-    {
-      id: 4,
-      title: this.$store.state.lang.printer.ste520.spec.size.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.size.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.size.value
-    },
-    {
-      id: 5,
-      title: this.$store.state.lang.printer.ste520.spec.volume.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.volume.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.volume.value
-    },
-    {
-      id: 6,
-      title: this.$store.state.lang.printer.ste520.spec.resolution.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.resolution.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.resolution.value
-    }, {
-      id: 7,
-      title: this.$store.state.lang.printer.ste520.spec.materials.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.materials.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.materials.value
-    },
-    {
-      id: 8,
-      title: this.$store.state.lang.printer.ste520.spec.diameter.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.diameter.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.diameter.value
-    },
-    {
-      id: 9,
-      title: this.$store.state.lang.printer.ste520.spec.nozzle.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.nozzle.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.nozzle.value
-    }, {
-      id: 10,
-      title: this.$store.state.lang.printer.ste520.spec.temperature.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.temperature.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.temperature.value
-    }, {
-      id: 11,
-      title: this.$store.state.lang.printer.ste520.spec.buildplate.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.buildplate.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.buildplate.value
-    }, {
-      id: 12,
-      title: this.$store.state.lang.printer.ste520.spec.connectivity.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.connectivity.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.connectivity.value
-    },
-    {
-      id: 13,
-      title: this.$store.state.lang.printer.ste520.spec.power.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.power.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.power.value
-    },
-    {
-      id: 14,
-      title: this.$store.state.lang.printer.ste520.spec.ambient.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.ambient.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.ambient.value
-    },
-    {
-      id: 15,
-      title: this.$store.state.lang.printer.ste520.spec.software.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.software.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.software.value
-    },
-    {
-      id: 16,
-      title: this.$store.state.lang.printer.ste520.spec.filetypes.key,
-      ste520: this.$store.state.lang.printer.ste520.spec.filetypes.value,
-      ste320: this.$store.state.lang.printer.ste320.spec.filetypes.value
-    }
+  compareTableData: any[] = []
 
-  ]
+  async mounted () {
+    let result = await this.$apollo.query({
+      query: gql`{
+        products {
+          product_id
+          name,
+          price,
+          options {
+            product_option_id,
+             product_option_value {
+                name, 
+                price
+           }
+          }
+        }
+      }`
+    })
+    this.products = result.data.products
+
+    this.compareTableData = this.products.map(product => {
+      if (product.options) {
+        let option = product.options.find(opt => opt.product_option_id == '394')
+        return [...option.product_option_value]
+
+      }
+    })
+    console.log(this.compareTableData)
+  }
 }
 
 </script>
--- pages/printers/index.vue
+++ pages/printers/index.vue
@@ -1,39 +1,41 @@
 <template>
-  <v-row>
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>Общее описание</v-card-title>
-        <v-card-text>Приемущества наших принтеров. Фото с Гришей</v-card-text>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>Карточки с кратким описанием принтера и слоганом</v-card-title>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>Таблица сравнения функций</v-card-title>
-      </v-card>
-    </v-col>
-    <v-col cols="12">
-      <v-card>
-        <v-card-title>Блог</v-card-title>
-        <v-card-text>Три последних поста</v-card-text>
-      </v-card>
-    </v-col>
-    <v-col class="align-center" cols="12">
-      <h1
-        class="display-3 text-uppercase font-weight-light text-center hidden-xs-only"
-      >{{ $store.state.lang.compareOurPrinters }}</h1>
-      <h1
-        class="headline text-uppercase font-weight-light text-center hidden-sm-and-up"
-      >{{ $store.state.lang.compareOurPrinters }}</h1>
-    </v-col>
-    <CompareBanner />
+  <v-container fluid>
+    <v-row justify="center">
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Общее описание</v-card-title>
+          <v-card-text>Приемущества наших принтеров. Фото с Гришей</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Карточки с кратким описанием принтера и слоганом</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Таблица сравнения функций</v-card-title>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card>
+          <v-card-title>Блог</v-card-title>
+          <v-card-text>Три последних поста</v-card-text>
+        </v-card>
+      </v-col>
+      <v-col class="align-center" cols="12">
+        <h1
+          class="display-3 text-uppercase font-weight-light text-center hidden-xs-only"
+        >{{ $store.state.lang.compareOurPrinters }}</h1>
+        <h1
+          class="headline text-uppercase font-weight-light text-center hidden-sm-and-up"
+        >{{ $store.state.lang.compareOurPrinters }}</h1>
+      </v-col>
+      <CompareBanner />
 
-    <CompareSpecs />
-  </v-row>
+      <CompareSpecs />
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
