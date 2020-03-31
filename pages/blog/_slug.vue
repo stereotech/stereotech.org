aad9cb179242e0c9218c84ae2133515946015aae
Ilya Gushchin <frylock3434@gmail.com>
2020-03-31 18:03:08 +0400

    add about stereotech page

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,25 +1,23 @@
 <template>
-    <v-container>
-        <h2>Блог</h2>
-        <h4>Категории</h4>
-        <v-row>
-            <v-chip-group>
-                <v-chip v-for="category in blog_categories" :key="category">
-                    {{category}}
-                </v-chip>
-            </v-chip-group>
-        </v-row>
-        <blogPostCard 
-            v-for="(post, index) in posts"
-            :key="index"
-            photoPath = "posts.image"
-            postTitle = "posts.name"
-            postDescription = "posts.description"
-        ></blogPostCard>
-        <v-row align="center">
-            <v-btn dark color="primary" @click="showMore()">Показать ещё</v-btn>
-        </v-row>
-    </v-container>
+  <v-container>
+    <h2>Блог</h2>
+    <h4>Категории</h4>
+    <v-row>
+      <v-chip-group>
+        <v-chip v-for="category in blog_categories" :key="category">{{category}}</v-chip>
+      </v-chip-group>
+    </v-row>
+    <blogPostCard
+      v-for="(post, index) in posts"
+      :key="index"
+      photoPath="posts.image"
+      postTitle="posts.name"
+      postDescription="posts.description"
+    ></blogPostCard>
+    <v-row align="center">
+      <v-btn dark color="primary" @click="showMore()">Показать ещё</v-btn>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -27,38 +25,38 @@ import { Vue, Component } from 'vue-property-decorator'
 import gql from 'graphql-tag'
 import blogPostCard from '~/components/blogPostCard.vue'
 @Component({
-    components:{
-        blogPostCard
-    }
+  components: {
+    blogPostCard
+  }
 })
 export default class Blog extends Vue {
 
-    private posts: any[] = []
+  private posts: any[] = []
 
-    private blog_categories: any[]=[]
-    async mounted(){
-        let result = await this.$apollo.query({
-            query: gql`query blog_filter(filter_category_id: "1", sort: "p.date_added" , order: "DESC", start: 0, limit: 6){
+  private blog_categories: any[] = []
+  async mounted () {
+    let result = await this.$apollo.query({
+      query: gql`query blog_filter(filter_category_id: "1", sort: "p.date_added" , order: "DESC", start: 0, limit: 6){
                 post_id,
                 name,
                 image,
                 description
             }`
-        })
-        this.posts = result.data.blog_filter
+    })
+    this.posts = result.data.blog_filter
 
-        let categories = await this.$apollo.query({
-            query: gql`query blog_category{
+    let categories = await this.$apollo.query({
+      query: gql`query blog_category{
                
                 name
             }`
-        })
-        this.blog_categories = categories.data.blog_category
-    }
+    })
+    this.blog_categories = categories.data.blog_category
+  }
 
-    private showMore(){
+  private showMore () {
 
-    }
+  }
 }
 
 </script>
