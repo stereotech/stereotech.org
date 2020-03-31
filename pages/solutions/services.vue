45e1f90ce9f578c535623490732b2248dc20b9e3
Ilya Gushchin <frylock3434@gmail.com>
2020-03-31 20:12:04 +0400

    add tips

---
+++ pages/solutions/services.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+
+@Component
+export default class Services extends Vue {
+
+}
+
+</script>
+
+<style>
+</style>
