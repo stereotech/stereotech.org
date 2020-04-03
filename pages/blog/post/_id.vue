b44671eb80d6652732a7b040a8ebd9115f4f5a19
Ilya Gushchin <frylock3434@gmail.com>
2020-04-03 19:32:37 +0400

    исправил загрузку поста

--- pages/blog/post/_id.vue
+++ pages/blog/post/_id.vue
@@ -1,21 +1,26 @@
 <template>
-    <v-container>
-        <v-row justify="center">
-            <v-col cols="12">
-                <v-parallax :src="`https://api.ste3d.ru/image/${post.image}`"></v-parallax>
-            </v-col>
-           <v-col class="text-center">
-                <h1 class="font-weight-light">{{post.name}}</h1>
-           </v-col>
-           <v-col offset-md="2">
-                <p>{{post.author}}</p>
-                <p>{{post.date_added}}</p>
-           </v-col>
-            <v-col class="text-center">
-                <p>{{post.description}}</p>
-            </v-col>
-        </v-row>
-    </v-container>
+  <v-container>
+    <v-row justify="center" v-if="post">
+      <v-col cols="12">
+        <v-parallax :src="`https://api.ste3d.ru/image/${post.image}`"></v-parallax>
+      </v-col>
+      <v-col class="text-center">
+        <h1 class="font-weight-light">{{post.name}}</h1>
+      </v-col>
+      <v-col offset-md="2">
+        <p>{{post.author}}</p>
+        <p>{{post.date_added}}</p>
+      </v-col>
+      <v-col cols="12">
+        <div v-html="post.description"></div>
+      </v-col>
+    </v-row>
+    <v-row v-else justify="center">
+      <v-col cols="1">
+        <v-progress-circular indeterminate></v-progress-circular>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -24,29 +29,30 @@ import gql from 'graphql-tag'
 
 @Component
 export default class BlogPost extends Vue {
-    @Prop({type: String, required: true, default: ''}) photoPath!: string
+  @Prop({ type: String, required: true, default: '' }) photoPath!: string
 
 
-    private post : any
-    //private blog_post_id: number = 0
-    async mounted(){
-        let result = await this.$apollo.query({
-            query: gql`query($id: ID!){
-                blog_post(id: $id){
-                name,
-                image,
-                author,
-                date_added,
-                description
+  private post: any = {}
+  //private blog_post_id: number = 0
+  async mounted () {
+    let result = await this.$apollo.query({
+      query: gql`query($id: ID!){
+                blog_post(id: $id) {
+                    name,
+                    image,
+                    author,
+                    date_added,
+                    description
                 }
             }`,
-                variables: {
-                    id: this.$route.params.id
-                }
-        })
-        this.post = result.data.blog_post
-        //console.log(this.$route.params)
-    }
+      variables: {
+        id: this.$route.params.id
+      }
+    })
+    this.post = result.data.blog_post
+    this.post.description = this.$convertHtml(this.post.description)
+    //console.log(this.$route.params)
+  }
 
 }
 
