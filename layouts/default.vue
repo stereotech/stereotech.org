1deb4ee8fafa80778906003b3cfcb0d3ae2dcf21
Ilya Gushchin <frylock3434@gmail.com>
2020-04-07 22:46:25 +0400

    reduce image size

--- layouts/default.vue
+++ layouts/default.vue
@@ -218,7 +218,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator'
 
 export interface MenuItem {
   title: string,
