cdd8ce985bdfde9bb3fd9715851485158c34392f
Kate <katya.romakh@yandex.ru>
2020-04-10 13:59:37 +0400

    edit description in PrinterSelector, prepared text for traslation (solutions/tables)

--- components/printers/PrinterSelector.vue
+++ components/printers/PrinterSelector.vue
@@ -8,7 +8,7 @@
         </v-col>
         <v-col cols="12" sm="6">
           <h4 class="display-1 text-uppercase font-weight-light text-center">{{ model.model }}</h4>
-          <p class="subtitle-1 text-center">{{ model.description }}</p>
+          <p class="title font-weight-light text-center">{{ model.description }}</p>
           <v-radio-group
             v-if="selectExtruder.length > 1"
             @change="changeExtruder"
