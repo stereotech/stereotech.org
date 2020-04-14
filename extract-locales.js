ef96c14b265270fd0b84b03fc10ab5b29a64604d
Ilya Gushchin <frylock3434@gmail.com>
2020-04-14 21:06:05 +0400

    update extraction script

--- extract-locales.js
+++ extract-locales.js
@@ -4,6 +4,8 @@ const fs = require('fs')
 
 const components_report = VueI18NExtract.createI18NReport('./components/**/*.?(vue)', './locales/*.?(json)')
 const components_items = VueI18NExtract.parseVueFiles('./components/**/*.?(vue)')
+const pages_report = VueI18NExtract.createI18NReport('./pages/**/*.?(vue)', './locales/*.?(json)')
+const items = VueI18NExtract.parseVueFiles('./pages/**/*.?(vue)')
 fs.readFile('locales/ru.json', (e, data) => {
     let lang = JSON.parse(data)
     components_items.forEach(item => {
@@ -12,18 +14,11 @@ fs.readFile('locales/ru.json', (e, data) => {
             lang[item.path] = item.path
         }
     });
-    fs.writeFile('ru_components_extracted.json', JSON.stringify(lang, null, ' '), () => { console.log('Locale extracted') })
-})
-
-const pages_report = VueI18NExtract.createI18NReport('./pages/**/*.?(vue)', './locales/*.?(json)')
-const items = VueI18NExtract.parseVueFiles('./pages/**/*.?(vue)')
-fs.readFile('locales/ru.json', (e, data) => {
-    let lang = JSON.parse(data)
     items.forEach(item => {
         const missing = pages_report.missingKeys.find(m => (m.path === item.path) && (m.language === 'ru'))
         if (missing) {
             lang[item.path] = item.path
         }
     });
-    fs.writeFile('ru_pages_extracted.json', JSON.stringify(lang, null, ' '), () => { console.log('Locale extracted') })
+    fs.writeFile('ru_extracted.json', JSON.stringify(lang, null, ' '), () => { console.log('Locale extracted') })
 })
