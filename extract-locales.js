5ab48eecd0463a55f6053af8aaac14bc456bad40
Ilya Gushchin <frylock3434@gmail.com>
2020-04-14 21:00:40 +0400

    обновил поиск переводов

--- extract-locales.js
+++ extract-locales.js
@@ -2,16 +2,28 @@ const VueI18NExtract = require('vue-i18n-extract').default;
 const fs = require('fs')
 
 
-const report = VueI18NExtract.createI18NReport('./components/**/*.?(vue)', './locales/*.?(json)')
-const items = VueI18NExtract.parseVueFiles('./components/**/*.?(vue)')
+const components_report = VueI18NExtract.createI18NReport('./components/**/*.?(vue)', './locales/*.?(json)')
+const components_items = VueI18NExtract.parseVueFiles('./components/**/*.?(vue)')
 fs.readFile('locales/ru.json', (e, data) => {
     let lang = JSON.parse(data)
-    items.forEach(item => {
-        const missing = report.missingKeys.find(m => (m.path === item.path) && (m.language === 'ru'))
+    components_items.forEach(item => {
+        const missing = components_report.missingKeys.find(m => (m.path === item.path) && (m.language === 'ru'))
         if (missing) {
             lang[item.path] = item.path
         }
     });
-    fs.writeFile('ru_extracted.json', JSON.stringify(lang, null, ' '), () => { console.log('Locale extracted') })
+    fs.writeFile('ru_components_extracted.json', JSON.stringify(lang, null, ' '), () => { console.log('Locale extracted') })
 })
 
+const pages_report = VueI18NExtract.createI18NReport('./pages/**/*.?(vue)', './locales/*.?(json)')
+const items = VueI18NExtract.parseVueFiles('./pages/**/*.?(vue)')
+fs.readFile('locales/ru.json', (e, data) => {
+    let lang = JSON.parse(data)
+    items.forEach(item => {
+        const missing = pages_report.missingKeys.find(m => (m.path === item.path) && (m.language === 'ru'))
+        if (missing) {
+            lang[item.path] = item.path
+        }
+    });
+    fs.writeFile('ru_pages_extracted.json', JSON.stringify(lang, null, ' '), () => { console.log('Locale extracted') })
+})
