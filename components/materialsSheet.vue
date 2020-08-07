200109ee88a803b21bb93281f2376306896fe337
Kate <katya.romakh@yandex.ru>
2020-08-07 14:27:31 +0400

    added search and filter for materials table

--- components/materialsSheet.vue
+++ components/materialsSheet.vue
@@ -1,6 +1,20 @@
 <template>
   <v-card>
-      <v-data-table :headers="tableHeaders" :items="tableData" :dense="true">
+      <v-card-title>
+          {{$t('Поддерживаемые материалы для печати')}}
+           <v-spacer></v-spacer>
+            <v-text-field
+                v-model="search"
+                append-icon="mdi-magnify"
+                label="Search"
+                single-line
+                hide-details
+            ></v-text-field>
+      </v-card-title>
+      <v-data-table :headers="tableHeaders" 
+        :items="tableData" 
+        :dense="true" 
+        :search="search">
 
       </v-data-table>
   </v-card>
@@ -16,13 +30,29 @@ export default class materialsSheet extends Vue{
 
 private tableData: any[] = []
 private tableHeaders: any[] = []
-
+private search: string = ""
 private async getSheetData(){
+
+    var removes: number[] = []
     const response = await fetch(url)
     
     const rows: string[][] = (await response.json()).values
-    
-    const headers = rows.shift()
+    console.log(rows.length)
+    for(var i = 1; i < rows[0].length; i++){
+    let dashCounter: number = 0
+        for (let j = 0; j<rows.length; j++){
+            if (rows[j][i] === '-'){
+                dashCounter++
+            }
+        }
+        if (dashCounter >= (rows.length-3)){
+            removes.push(i)
+        }
+    }
+    var newRows = rows.map(function(val, ind){
+        return val.filter(function(val, ind) { return !removes.includes(ind); });
+    });
+    const headers = newRows.shift()
     if (headers) {
         this.tableHeaders = headers.map(h => {
             return {
@@ -33,15 +63,15 @@ private async getSheetData(){
     } else {
         this.tableHeaders = []
     }
-    console.log(this.tableHeaders)
-    this.tableData = rows.map(r => {
+    //console.log(this.tableHeaders)
+    this.tableData = newRows.map(r => {
         const entries = new Map(r.map((v, index) => {
             return [this.tableHeaders[index].value, v]
         }))
         const item = Object.fromEntries(entries)
         return item
     })
-    console.log(this.tableData)
+    //console.log(this.tableData)
 
 }
 
