639d61bc7ecfd7a58485fa5315822f3f78c79433
Ilya Gushchin <frylock3434@gmail.com>
2020-10-07 11:51:39 +0400

    add dynamic items

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,13 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" app clipped temporary hide-overlay width="300">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      temporary
+      hide-overlay
+      width="300"
+    >
       <v-list nav>
         <template v-for="(menuItem, index) in mainMenu">
           <v-list-group value="true" :key="index" :prepend-icon="menuItem.icon">
@@ -9,8 +16,15 @@
                 <v-list-item-title>{{ menuItem.title }}</v-list-item-title>
               </v-list-item-content>
             </template>
-            <template v-for="(subItem, subIndex) in mapMenuItems(menuItem.child)">
-              <v-list-item :key="subIndex" nuxt :to="localePath(subItem.link)" exact>
+            <template
+              v-for="(subItem, subIndex) in mapMenuItems(menuItem.child)"
+            >
+              <v-list-item
+                :key="subIndex"
+                nuxt
+                :to="localePath(subItem.link)"
+                exact
+              >
                 <v-list-item-content>
                   <v-list-item-title>{{ subItem.title }}</v-list-item-title>
                 </v-list-item-content>
@@ -22,12 +36,15 @@
           <v-list-item-icon>
             <v-icon>mdi-map-marker-question-outline</v-icon>
           </v-list-item-icon>
-          <v-list-item-title>{{$t('Где купить')}}</v-list-item-title>
+          <v-list-item-title>{{ $t("Где купить") }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
     <v-app-bar color="secondary" app clipped-left>
-      <v-app-bar-nav-icon class="hidden-md-and-up" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon
+        class="hidden-md-and-up"
+        @click.stop="drawer = !drawer"
+      ></v-app-bar-nav-icon>
       <v-avatar size="36px" class="mr-2">
         <img src="ste-logo.png" alt="Logo" />
       </v-avatar>
@@ -57,16 +74,23 @@
                   <template v-for="(childItem, childIndex) in menuItem.child">
                     <v-col cols="4" :key="childIndex">
                       <v-list dense nav class="primary--text">
-                        <v-list-item nuxt exact :to="localePath(childItem.link)">
+                        <v-list-item
+                          nuxt
+                          exact
+                          :to="localePath(childItem.link)"
+                        >
                           <v-list-item-avatar tile v-if="childItem.icon">
                             <v-icon
                               color="primary"
                               v-if="childItem.icon.startsWith('mdi-')"
-                            >{{ childItem.icon }}</v-icon>
+                              >{{ childItem.icon }}</v-icon
+                            >
                             <v-img contain v-else :src="childItem.icon"></v-img>
                           </v-list-item-avatar>
                           <v-list-item-content>
-                            <v-list-item-title>{{ childItem.title }}</v-list-item-title>
+                            <v-list-item-title>{{
+                              childItem.title
+                            }}</v-list-item-title>
                           </v-list-item-content>
                         </v-list-item>
                         <v-divider v-if="childItem.child" />
@@ -78,8 +102,12 @@
                           :to="localePath(sublink.link)"
                         >
                           <v-list-item-content>
-                            <v-list-item-title class="caption">{{ sublink.title }}</v-list-item-title>
-                            <v-list-item-subtitle>{{ sublink.description }}</v-list-item-subtitle>
+                            <v-list-item-title class="caption">{{
+                              sublink.title
+                            }}</v-list-item-title>
+                            <v-list-item-subtitle>{{
+                              sublink.description
+                            }}</v-list-item-subtitle>
                           </v-list-item-content>
                         </v-list-item>
                       </v-list>
@@ -95,8 +123,16 @@
             text
             nuxt
             :to="localePath(menuItem.link)"
-          >{{ menuItem.title }}</v-btn>
-          <v-btn v-else :key="index" text :href="menuItem.link" target="_blank">{{ menuItem.title }}</v-btn>
+            >{{ menuItem.title }}</v-btn
+          >
+          <v-btn
+            v-else
+            :key="index"
+            text
+            :href="menuItem.link"
+            target="_blank"
+            >{{ menuItem.title }}</v-btn
+          >
         </template>
       </v-toolbar-items>
       <v-spacer />
@@ -108,7 +144,7 @@
         exact
         class="hidden-sm-and-down"
       >
-        {{$t('Где купить')}}
+        {{ $t("Где купить") }}
         <v-icon right dark>mdi-map-marker-question-outline</v-icon>
       </v-btn>
       <v-menu offset-y>
@@ -129,7 +165,8 @@
             v-for="locale in $i18n.locales"
             :key="locale.code"
             :to="switchLocalePath(locale.code)"
-          >{{ locale.name }}</v-list-item>
+            >{{ locale.name }}</v-list-item
+          >
         </v-list>
       </v-menu>
     </v-app-bar>
@@ -157,10 +194,20 @@
                       <v-list-item-title>info@5dtech.pro</v-list-item-title>
                     </v-list-item>
                     <v-list-item>
-                      <v-btn text icon href="https://vk.com/5dtechpro" target="_blank">
+                      <v-btn
+                        text
+                        icon
+                        href="https://vk.com/5dtechpro"
+                        target="_blank"
+                      >
                         <v-icon>mdi-vk</v-icon>
                       </v-btn>
-                      <v-btn text icon href="https://instagram.com/5dtechpro/" target="_blank">
+                      <v-btn
+                        text
+                        icon
+                        href="https://instagram.com/5dtechpro/"
+                        target="_blank"
+                      >
                         <v-icon>mdi-instagram</v-icon>
                       </v-btn>
                       <v-btn
@@ -180,10 +227,19 @@
           <v-col cols="12" sm="9">
             <v-container>
               <v-row>
-                <v-col cols="12" sm="6" lg="3" v-for="(menu, index) in mainMenu" :key="index">
+                <v-col
+                  cols="12"
+                  sm="6"
+                  lg="3"
+                  v-for="(menu, index) in mainMenu"
+                  :key="index"
+                >
                   <v-list flat light dense dark color="primary">
                     <v-list-item>
-                      <v-list-item-title class="text-uppercase font-weight-bold">{{ menu.title }}</v-list-item-title>
+                      <v-list-item-title
+                        class="text-uppercase font-weight-bold"
+                        >{{ menu.title }}</v-list-item-title
+                      >
                     </v-list-item>
                     <v-divider />
                     <v-list-item
@@ -192,21 +248,25 @@
                       v-for="(child, childIndex) in menu.child"
                       :key="childIndex"
                     >
-                      <v-list-item-title>{{ child.title}}</v-list-item-title>
+                      <v-list-item-title>{{ child.title }}</v-list-item-title>
                     </v-list-item>
                     <v-list-item
-                      v-if="index==3"
+                      v-if="index == 3"
                       href="http://download.stereotech.org/documents/%D0%A0%D0%B5%D0%BA%D0%B2%D0%B8%D0%B7%D0%B8%D1%82%D1%8B.pdf"
                       target="_blank"
                     >
-                      <v-list-item-title>{{$t('Реквизиты')}}</v-list-item-title>
+                      <v-list-item-title>{{
+                        $t("Реквизиты")
+                      }}</v-list-item-title>
                     </v-list-item>
                     <v-list-item
-                      v-if="index==3"
+                      v-if="index == 3"
                       href="http://download.stereotech.org/documents/2020+%D0%A0%D0%B5%D0%B7%D1%83%D0%BB%D1%8C%D1%82%D0%B0%D1%82%D1%8B+%D0%A1%D0%9E%D0%A3%D0%A2.pdf"
                       target="_blank"
                     >
-                      <v-list-item-title>{{$t('Результаты СОУТ')}}</v-list-item-title>
+                      <v-list-item-title>{{
+                        $t("Результаты СОУТ")
+                      }}</v-list-item-title>
                     </v-list-item>
                   </v-list>
                 </v-col>
@@ -261,8 +321,8 @@ export default class Layout extends Vue {
     return items.flatMap(x => x.child ? [x, ...x.child] : [x])
   }
 
-  async mounted () {
-    this.mainMenu.push(
+  get mainMenu (): MenuItem[] {
+    return [
       {
         title: this.$tc("Продукты"),
         icon: 'mdi-printer-3d',
@@ -329,56 +389,15 @@ export default class Layout extends Vue {
         title: this.$tc('Узнать больше'),
         icon: 'mdi-post-outline',
         child: [
-          //{
-          //  title: this.$tc('Решения'),
-          //  link: '/solutions',
-          //  child: [
-          //    {
-          //      title: this.$tc('Обучение'),
-          //      description: this.$tc('Изучение основ 3D печати'),
-          //      link: '/solutions/education'
-          //    },
-          //    {
-          //      title: this.$tc('Производство'),
-          //      description: this.$tc('Изделия для конечного использования'),
-          //      link: '/solutions/production'
-          //    },
-          //    {
-          //      title: this.$tc('Сервис 3D печати'),
-          //      description: this.$tc('Услуги 3D печати и прототипирования'),
-          //      link: '/solutions/services'
-          //    },
-          //    {
-          //      title: this.$tc('Прототипирование'),
-          //      description: this.$tc('Разработка устройств'),
-          //      link: '/solutions/prototyping'
-          //    },
-          //  ]
-          //},
           {
             title: this.$tc('Блог'),
             link: '/blog',
             child: [
-              //{
-              //  title: this.$tc('Анонсы'),
-              //  description: this.$tc('Все важные мероприятия с нашим участием'),
-              //  link: '/blog/events'
-              //},
               {
                 title: this.$tc('Новости'),
                 description: this.$tc('Последние новости о нашей компании'),
                 link: '/blog/news'
               },
-              //{
-              //  title: this.$tc('Примеры использования'),
-              //  description: this.$tc('Истории успеха наших партнеров'),
-              //  link: '/blog/applications'
-              //},
-              //{
-              //  title: this.$tc('Статьи'),
-              //  description: this.$tc('Подробные исследования и руководства'),
-              //  link: '/blog/whitepapers'
-              //},
               {
                 title: this.$tc('Научная деятельность'),
                 description: '',
@@ -415,28 +434,6 @@ export default class Layout extends Vue {
               }
             ]
           },
-          //{
-          //  title: this.$tc('Советы по использованию'),
-          //  link: '/support/tips',
-          //  child: [
-          //    {
-          //      title: this.$tc('Принтеры'),
-          //      link: '/support/tips/printers'
-          //    },
-          //    {
-          //      title: this.$tc('Программное обеспечение'),
-          //      link: '/support/tips/software'
-          //    },
-          //    {
-          //      title: this.$tc('Материалы'),
-          //      link: '/support/tips/materials'
-          //    },
-          //    {
-          //      title: this.$tc('Печать'),
-          //      link: '/support/tips/printing'
-          //    }
-          //  ]
-          //},
           {
             title: this.$tc('Связаться с нами'),
             link: '/support'
@@ -455,10 +452,6 @@ export default class Layout extends Vue {
                 title: this.$tc('Команда'),
                 link: '/info/team'
               },
-              //{
-              //  title: this.$tc('Награды'),
-              //  link: '/info/awards'
-              //},
               {
                 title: this.$tc('Адреса'),
                 link: '/info/address'
@@ -471,220 +464,9 @@ export default class Layout extends Vue {
           }
         ]
 
-      })
+      }
+    ]
   }
-  private mainMenu: MenuItem[] = [
-    // {
-    //   title: "Продукты",
-    //   icon: 'mdi-printer-3d',
-    //   child: [
-    //     {
-    //       title: 'Настольные принтеры',
-    //       link: '/printers',
-    //       icon: '/printers/desktop/series3.jpg',
-    //       child: [
-    //         {
-    //           title: 'Ceрия 3xx',
-    //           link: '/printers/series3',
-    //           description: 'Профессиональные 3D принтеры'
-    //         },
-    //         {
-    //           title: 'Ceрия 5xx',
-    //           link: '/printers/series5',
-    //           description: 'Инновационные 5D принтеры'
-    //         },
-    //         {
-    //           title: 'Серия Special',
-    //           link: '/printers/special',
-    //           description: '3D и 5D принтеры для особых задач'
-    //         }
-    //       ]
-    //     },
-    //     {
-    //       title: 'Промышленные принтеры',
-    //       link: '/industrial',
-    //       icon: 'printers/industrial/series8.jpg',
-    //       child: [
-    //         {
-    //           title: 'Ceрия 6xx',
-    //           link: '/industrial/series6',
-    //           description: 'Шестиосевое устройство на базе промышленного робота'
-    //         },
-    //         {
-    //           title: 'Ceрия 8xx',
-    //           link: '/industrial/series8',
-    //           description: 'Восьмиосевое устройство для специальных задач'
-    //         }
-    //       ]
-    //     },
-    //     {
-    //       title: 'Программное обеспечение',
-    //       link: '/software',
-    //       icon: 'printers/software/steapp.webp',
-    //       child: [
-    //         {
-    //           title: 'STE Slicer',
-    //           link: '/software/steslicer',
-    //           description: 'Подготовка к 3D и 5D печати'
-    //         },
-    //         {
-    //           title: 'STE App',
-    //           link: '/software/steapp',
-    //           description: 'Управление процессом печати'
-    //         },
-    //       ]
-    //     },
-    //   ]
-    // },
-    // {
-    //   title: 'Узнать больше',
-    //   icon: 'mdi-post-outline',
-    //   child: [
-    //     //{
-    //     //  title: 'Решения',
-    //     //  link: '/solutions',
-    //     //  child: [
-    //     //    {
-    //     //      title: 'Обучение',
-    //     //      description: 'Изучение основ 3D печати',
-    //     //      link: '/solutions/education'
-    //     //    },
-    //     //    {
-    //     //      title: 'Производство',
-    //     //      description: 'Изделия для конечного использования',
-    //     //      link: '/solutions/production'
-    //     //    },
-    //     //    {
-    //     //      title: 'Сервис 3D печати',
-    //     //      description: 'Услуги 3D печати и прототипирования',
-    //     //      link: '/solutions/services'
-    //     //    },
-    //     //    {
-    //     //      title: 'Прототипирование',
-    //     //      description: 'Разработка устройств',
-    //     //      link: '/solutions/prototyping'
-    //     //    },
-    //     //  ]
-    //     //},
-    //     {
-    //       title: 'Блог',
-    //       link: '/blog',
-    //       child: [
-    //         //{
-    //         //  title: 'Анонсы',
-    //         //  description: 'Все важные мероприятия с нашим участием',
-    //         //  link: '/blog/events'
-    //         //},
-    //         {
-    //           title: 'Новости',
-    //           description: 'Последние новости о нашей компании',
-    //           link: '/blog/news'
-    //         },
-    //         //{
-    //         //  title: 'Примеры использования',
-    //         //  description: 'Истории успеха наших партнеров',
-    //         //  link: '/blog/applications'
-    //         //},
-    //         //{
-    //         //  title: 'Статьи',
-    //         //  description: 'Подробные исследования и руководства',
-    //         //  link: '/blog/whitepapers'
-    //         //},
-    //         {
-    //           title: 'Научная деятельность',
-    //           description: '',
-    //           link: '/blog/science'
-    //         },
-    //       ]
-    //     }
-    //   ]
-    // },
-    // {
-    //   title: 'Поддержка',
-    //   icon: 'mdi-face-agent',
-    //   child: [
-
-    //     {
-    //       title: 'Руководства пользователя',
-    //       link: '/support/manuals',
-    //       child: [
-    //         {
-    //           title: 'Принтеры 3хх серии',
-    //           link: '/support/manuals/ste320'
-    //         },
-    //         //{
-    //         //  title: 'Принтеры 5хх серии',
-    //         //  link: '/support/manuals/ste520'
-    //         //},
-    //         {
-    //           title: 'Слайсер STE Slicer',
-    //           link: '/support/manuals/steslicer'
-    //         },
-    //         {
-    //           title: 'Система управления STE App',
-    //           link: '/support/manuals/steapp'
-    //         }
-    //       ]
-    //     },
-    //     //{
-    //     //  title: 'Советы по использованию',
-    //     //  link: '/support/tips',
-    //     //  child: [
-    //     //    {
-    //     //      title: 'Принтеры',
-    //     //      link: '/support/tips/printers'
-    //     //    },
-    //     //    {
-    //     //      title: 'Программное обеспечение',
-    //     //      link: '/support/tips/software'
-    //     //    },
-    //     //    {
-    //     //      title: 'Материалы',
-    //     //      link: '/support/tips/materials'
-    //     //    },
-    //     //    {
-    //     //      title: 'Печать',
-    //     //      link: '/support/tips/printing'
-    //     //    }
-    //     //  ]
-    //     //},
-    //     {
-    //       title: 'Связаться с нами',
-    //       link: '/support'
-    //     },
-    //   ]
-    // },
-    // {
-    //   title: 'О нас',
-    //   icon: 'mdi-information',
-    //   child: [
-    //     {
-    //       title: 'О Stereotech',
-    //       link: '/info',
-    //       child: [
-    //         {
-    //           title: 'Команда',
-    //           link: '/info/team'
-    //         },
-    //         //{
-    //         //  title: 'Награды',
-    //         //  link: '/info/awards'
-    //         //},
-    //         {
-    //           title: 'Адреса',
-    //           link: '/info/address'
-    //         },
-    //         {
-    //           title: 'СМИ о нас',
-    //           link: '/info/media'
-    //         }
-    //       ]
-    //     }
-    //   ]
-
-    // }
-
-  ]
   private miniVariant: boolean = this.$vuetify.breakpoint.smOnly
   private drawer: boolean = false
 
