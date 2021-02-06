547190049d21da241620cef23b6343249d0d9b8c
Kate8954 <katya.romakh@yandex.ru>
2021-02-06 17:44:48 +0300

    getting keyFeatures fron cockpit on series8xx

--- pages/industrial/series8.vue
+++ pages/industrial/series8.vue
@@ -44,6 +44,7 @@ import gql from 'graphql-tag'
   }
 })
 export default class Series8 extends Vue {
+  private features: any[] = []
   get currentPrinter (): PrinterVariant {
     return {
       model: this.$tc('Серия 8хх'),
@@ -55,65 +56,18 @@ export default class Series8 extends Vue {
       description: this.$tc('Восьмиосевая машина АП на базе промышленного манипулятора для производства прочных полимерных деталей')
     }
   }
-  get features (): KeyFeature[] {
-    return [
-      {
-        name: this.$tc('Прочность изделий'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/strengthDiagram.jpg',
-        description: this.$tc('Благодаря свойству ориентации полимеров, технология 5Dtech повышает прочность на изгиб и сжатие. Получайте детали:'),
-        keys: [this.$tc('До х4 раз прочнее, чем 3D'), this.$tc('на 32% прочнее, чем литье*')]
-      },
-      {
-        name: this.$tc('Армированная структура'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/3dStructure.jpg',
-        description: this.$tc('Благодаря объемной, а не послойной укладке нитей, технология 5Dtech армирует структуру изделия. Используйте это преимущество при печати непрерывными волокнами и создавайте беспрецедентно прочные квазиизотропные изделия.'),
-        keys: []
-      },
-      {
-        name: this.$tc('Печать без поддержек'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/dd.jpg',
-        description: this.$tc('Благодаря многоосевой кинематике, технология 5Dtech  позволяет либо полностью обойтись, либо существенно сократить поддержки под нависающими элементами изделия, что повышает скорость производства, сокращает время на постобработку и обеспечивает экономию материала до 70%.'),
-        keys: []
-      },
-      {
-        name: this.$tc('Высокая скорость'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/speed.jpg',
-        description: this.$tc('За счет оптимизации путей перемещения (более длинных участков заполнения, на которых принтер успевает набрать заданную скорость) скорость 5D печати выше до 2 раз, по сравнению с 3D печатью.'),
-        keys: []
-      },
-      {
-        name: this.$tc('Печать по любой поверхности'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/printingOnRig.jpg',
-        description: this.$tc('В отличие от классической 3D печати, технология 5Dtech может печатать на любой поверхности. Используйте специальную оснастку для серийного производства изделий или заготовку, которая станет частью будущего изделия – теперь Вы можете создавать композитные детали с металлическим армированием.'),
-        keys: []
-      },
-      {
-        name: this.$tc('Простота использования'),
-        mediaType: MediaType.video,
-        mediaSource: '/printers/Stereotech STE Slicer 2020-04-13 12-28-09.mp4',
-        description: this.$tc('Алгоритмы технологии 5Dtech обеспечивают автоматическую подготовку деталей к многоосевой печати, в отличие от промышленных решений, где требуется сложная ручная CAM подготовка управляющей программы. Печатать также просто, как на обычном 3D принтере.'),
-        keys: []
-      },
-      {
-        name: this.$tc('Без ограничений'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/CAD mesh model.jpg',
-        description: this.$tc('Используйте привычные программы для CAD моделирования. Программное обеспечение STE Slicer работает с общим форматом для 3D печати – STL, в отличие от промышленных решений, которые работают только с проприетарными форматами CAD моделей при подготовке к многоосевой печати.'),
-        keys: []
-      },
-      {
-        name: this.$tc('Широкий набор возможностей'),
-        mediaType: MediaType.image,
-        mediaSource: '/printers/printingModes 5D tech.jpg',
-        description: this.$tc('5D принтер и программное обеспечение STE Slicer позволяют работать с несколькими режимами печати и комбинировать их. Используйте режимы Классический / Навивка /С изменением направления /По поверхности для решения любого спектра задач.'),
-        keys: []
-      }
-    ]
+ 
+   private async getFeatures () {
+    let data1
+    let response1 = await fetch(`https://api2.stereotech.org/api/collections/get/features?token=${process.env.COCKPIT_TOKEN}`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        lang: this.$i18n.locale
+      })
+    })
+    data1 = await response1.json()
+    this.features = data1.entries
   }
 
   product: any = {}
@@ -146,6 +100,7 @@ export default class Series8 extends Vue {
       }`
     })
     this.product = result.data.product
+    await this.getFeatures()
   }
 }
 
