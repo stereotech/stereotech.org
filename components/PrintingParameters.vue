8f8e564c074731239724ad09d138f367d0bbf837
Kate8954 <katya.romakh@yandex.ru>
2021-03-24 13:31:27 +0300

    category index page

---
+++ components/PrintingParameters.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container fluid>
+      <v-data-iterator>
+          <template v-slot:default="props">
+              <v-row>
+                  <v-col v-for="item in props.items"
+                    :key="item.name"
+                    cols="12"
+                    sm="6"
+                    md="4"
+                    lg="3"
+                  >
+                    <v-card>
+                        <v-card-title></v-card-title>
+                        <v-divider></v-divider>
+                    </v-card>
+                  </v-col>
+              </v-row>
+          </template>
+      </v-data-iterator>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator'
+
+@Component
+export default class PrintingParameters extends Vue{
+    @Prop({})
+
+}
+</script>
+
+<style>
+
+</style>
--- pages/support/index.vue
+++ pages/support/index.vue
@@ -1,58 +1,39 @@
 <template>
-  <v-card>
-    <v-img
-      :src="require('~/static/support/series5_banner.jpg?webp')"
-      class="white--text align-center text-center"
-      :gradient="'to bottom, rgba(2,119,189,0), rgba(2, 119, 189, 0.8), rgba(2,119,189,0)'"
-    >
-      <h4 class="display-1 font-weight-light">
-        {{ $t("Руководства пользователя") }}
-      </h4>
-      <v-form>
-        <v-container>
-          <v-row>
-            <v-col cols="12" sm="10">
-              <v-text-field
-                solo
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" sm="2">  
-              <v-btn color="accent" block large>
-                <v-icon>mdi-magnify</v-icon>
-                {{$t("Поиск")}}
-              </v-btn>
-            </v-col>
-          </v-row>
-        </v-container>
-      </v-form>
-    </v-img>
-    <v-card-title></v-card-title>
+  <v-card >
+    <SupportBanner
+      :title="this.$tc('Руководства пользователя')"
+    />
     <v-card-text>
       <v-container fluid>
         <v-row justify="center">
-            <v-col
-              v-for="(category, index) in manualCategories"
-              :key="index"
-              cols="12"
-              md="3"
-            >
-              <v-card>
-                <v-card-title>{{ category.title }}</v-card-title>
-                <v-card-text>
-                  <v-img :src="category.image"></v-img>
-                </v-card-text>
-              </v-card>
-            </v-col>
-        </v-row>
-      </v-container>
+          <v-col
+            v-for="(category, index) in manualCategories"
+            :key="index"
+            cols="12"
+            md="3"
+          >
+            <v-card :to="``" nuxt>
+              <v-card-title>{{ category.title }}</v-card-title>
+              <v-card-text>
+                <v-img :src="category.image"></v-img>
+              </v-card-text>
+            </v-card>
+          </v-col>
+        </v-row> 
+      </v-container>       
     </v-card-text>
   </v-card>
 </template>
 
 <script lang="ts">
 import { Vue, Component } from "vue-property-decorator";
+import SupportBanner from "~/components/SupportBanner.vue"
 
-@Component
+@Component({
+  components:{
+    SupportBanner
+  }
+})
 export default class Support extends Vue {
   get manualCategories() {
     return [
@@ -65,6 +46,10 @@ export default class Support extends Vue {
         image: "support/steslicer.jpg",
       },
     ];
+  }
+
+  async mounted(){
+    
   }
 }
 </script>
