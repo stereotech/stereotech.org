0719679957ba6cd9be51f4da9970089c926b9cb5
Kate8954 <katya.romakh@yandex.ru>
2021-06-03 13:10:13 +0300

    closes #112 issue

--- pages/printers/fiber.vue
+++ pages/printers/fiber.vue
@@ -15,6 +15,40 @@
         />
       </v-col> 
       <v-col cols="12" lg="10">
+        <KeyFeatures
+          :title="$tc('Преимущества технологии 5Dtech')"
+          :items="features"
+        />
+      </v-col>
+      <v-col cols="12" lg="10">
+        <KeyFeatures
+          :title="this.$tc('Причины выбрать принтеры Stereotech')"
+          :items="reasonsToUse"
+        />
+      </v-col>
+      <v-col cols="12" lg="10">
+        <ServiceBenefits />
+      </v-col>
+      <v-col cols="12" lg="10">
+        <ProductCard
+          :image="require('~/static/materials/sealant.jpg?webp')"
+          :title="this.$tc('Материалы для печати')"
+          link="/materials"
+        />
+      </v-col>
+      <v-col cols="12" lg="10">
+        <ProductCard
+          image="/software/steslicer/banner.jpg"
+          title="STE Slicer"
+          :description="
+            $t(
+              'Первое в мире программное обеспечение для 5D принтеров\nПодготовка моделей для 3D и 5D печати\nПоддержка различных режимов 5D печати\nВстроенное STE App'
+            )
+          "
+          link="/software/steslicer"
+        />
+      </v-col>                              
+      <v-col cols="12" lg="10">
         <FullSpecs :specXd="spec5d" />
       </v-col>   
       <v-col cols="12" lg="10">
@@ -30,6 +64,9 @@ import BuyPrinter from '~/components/printers/BuyPrinter.vue'
 import PrinterSelector from '~/components/printers/PrinterSelector.vue'
 import FullSpecs from '~/components/printers/FullSpecs.vue'
 import MaterialsTable from '~/components/MaterialsTable.vue'
+import ServiceBenefits from '~/components/printers/ServiceBenefits.vue'
+import KeyFeatures from '~/components/KeyFeatures.vue'
+import ProductCard from '~/components/ProductCard.vue'
 import { PrinterVariant, ExtruderType, PrintVolumeType, FiveAxisType, PrinterType } from '~/types/printerVariant'
 import gql from 'graphql-tag'
 import { Material, MaterialSpec } from '~/types/materials'
@@ -42,7 +79,10 @@ const materials = namespace('materials')
     BuyPrinter,
     MaterialsTable,
     PrinterSelector,
-    FullSpecs
+    FullSpecs,
+    ServiceBenefits,
+    KeyFeatures,
+    ProductCard
   }
 })
 export default class Fiber extends Vue {
@@ -51,9 +91,37 @@ export default class Fiber extends Vue {
   @materials.Getter ourBrandMaterials!: any
   @materials.Getter specs!: MaterialSpec[]
 
+  private features: any[] = []
+  private reasonsToUse: any[] = []
   currentPrinter: any = {}
   private printerItems: any[] = []
   private spec5d: any[] = []
+
+  private async getFeatures () {
+    let data1
+    let response1 = await fetch(`https://api2.stereotech.org/api/collections/get/features?token=${process.env.COCKPIT_TOKEN}`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        lang: this.$i18n.locale
+      })
+    })
+    data1 = await response1.json()
+    this.features = data1.entries
+  }
+
+  private async getReasonsToUse () {
+    let data2
+    let response2 = await fetch(`https://api2.stereotech.org/api/collections/get/reasonsToChose?token=${process.env.COCKPIT_TOKEN}`,{
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        lang: this.$i18n.locale
+      })
+    })
+    data2 = await response2.json()
+    this.reasonsToUse = data2.entries
+  }
 
   private async getFulSpec5 () {
     let data
@@ -85,6 +153,8 @@ export default class Fiber extends Vue {
   }
   async mounted(){
     await this.getPrinterItems()
+    await this.getFeatures()
+    await this.getReasonsToUse()
     await this.getFulSpec5()
     if (!this.filled) {
       await this.loadMaterialsData()
