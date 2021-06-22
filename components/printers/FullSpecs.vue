9ccb150d8d5fd901ed407283d0071a602dd5ff2a
Ilya Gushchin <frylock3434@gmail.com>
2021-06-22 09:13:27 +0300

    Update FullSpecs.vue

--- components/printers/FullSpecs.vue
+++ components/printers/FullSpecs.vue
@@ -3,13 +3,11 @@
     <v-container>
       <v-row justify="center">
         <v-col cols="12">
-          <h4
-            class="headline text-uppercase font-weight-regular text-center"
-          >{{$t('Характеристики')}}</h4>
+          <h4 class="headline text-uppercase font-weight-regular text-center">
+            {{ $t("Характеристики") }}
+          </h4>
         </v-col>
-        <v-col
-          cols="12"
-        >
+        <v-col cols="12">
           <v-data-table
             :headers="getTableHeaders"
             :items="tableData"
@@ -34,38 +32,37 @@ export default class FullSpecs extends Vue {
   @Prop({ type: Array, required: true, default: () => { return [] } }) specXd!: any[]
 
   private datamap = new Map()
-  private tableHeaders: any[] = [{text:'характеристика',value:'spec'}]
+  private tableHeaders: any[] = [{ text: 'характеристика', value: 'spec' }]
   private tableData: any[] = []
 
-  private get getTableHeaders(){
+  private get getTableHeaders () {
     this.specXd.forEach(s => {
-    this.tableHeaders.push({ text:s.model, value:s.model})
-    let smodel=s.model
-    s.specs.forEach(el => {
-      let dataobject=this.datamap.get(el.value.key)
-      if(dataobject===undefined)
-      {
-        this.datamap.set(el.value.key,{})
-        dataobject=this.datamap.get(el.value.key)
-      }
-      
-      dataobject[smodel]=el.value.value
-      
+      this.tableHeaders.push({ text: s.model, value: s.model })
+      let smodel = s.model
+      s.specs.forEach(el => {
+        let dataobject = this.datamap.get(el.value.key)
+        if (dataobject === undefined) {
+          this.datamap.set(el.value.key, {})
+          dataobject = this.datamap.get(el.value.key)
+        }
+
+        dataobject[smodel] = el.value.value
+
+      });
     });
-    }); 
     this.getData()
     return this.tableHeaders
   }
-  private getData(){
-    
-    this.datamap.forEach((v,k) => {
-      this.tableData.push(Object.assign({spec:k},v))
+  private getData () {
+
+    this.datamap.forEach((v, k) => {
+      this.tableData.push(Object.assign({ spec: k }, v))
     });
     return this.tableData
   }
 
-  
-  async mounted(){
+
+  async mounted () {
 
   }
 
